1c40b32cbf1ccb8bcc6c8991befeb7ad6194f82d
Ilya Dutsev <elijah.dutsev@gmail.com>
2019-12-02 01:53:08 +0300

    final commit

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+    [SerializeField] Text topText;
+
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+
+        guess = Random.Range(min, max + 1);
+        guessText.text = "Загадал? Это " + guess.ToString() + "?";
+    }
+
+
+    public void OnHigherPress()
+    {
+        min = guess + 1;
+        if (min <= max)
+        {
+            NextGuess();
+        }
+
+    }
+    public void OnLowerPress()
+    {
+        max = guess;
+        if (min <= max)
+        {
+            NextGuess();
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = "Это " + guess.ToString() + "?";
+        topText.text = "";
+    }
+}
